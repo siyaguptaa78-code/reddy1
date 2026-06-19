914bed7f67dc7a57e9c07c732162e0facddfeb31
Aapka Naam <aapka.email@example.com>
2026-06-20 00:35:24 +0530

    feat: Integrate Google Tag Manager (GTM) tracking tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,12 +74,26 @@ export default function RootLayout({
   return (
     <html lang="en-US">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WXD7FFNS');`
+          }}
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap"
           rel="stylesheet"
         />
       </head>
       <body className="min-h-screen bg-[#05080f] text-gray-200 antialiased flex flex-col justify-between">
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WXD7FFNS" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+          }}
+        />
         <Header />
         <main className="flex-1 w-full bg-[#05080f]">{children}</main>
         <Footer />
