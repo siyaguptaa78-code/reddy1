c7df699f517c8fc9ab5c059cef6ce9720d4f0548
Jatin Bansal <jatin@altruistindia.com>
2026-09-16 11:10:10 +0530

    Revert "blog added"
    This reverts commit 8740f759722b257422ebceb64c5066f3a7b05d75.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,7 +9,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
     { url: `${baseUrl}/about-us`, lastModified, changeFrequency: 'monthly', priority: 0.8 },
     { url: `${baseUrl}/blogs`, lastModified, changeFrequency: 'weekly', priority: 0.8 },
     { url: `${baseUrl}/reddynow-guide`, lastModified, changeFrequency: 'weekly', priority: 0.8 },
-    { url: `${baseUrl}/european-open-2026-atp-guide`, lastModified, changeFrequency: 'weekly', priority: 0.8 },
     { url: `${baseUrl}/the-hundred-2026-schedule`, lastModified, changeFrequency: 'weekly', priority: 0.8 },
     { url: `${baseUrl}/contact-us`, lastModified, changeFrequency: 'monthly', priority: 0.7 },
     { url: `${baseUrl}/indian-card-games`, lastModified, changeFrequency: 'weekly', priority: 0.8 },
